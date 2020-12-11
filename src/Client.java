d18bbc585403344f5b018492381a1597afd501fc
MHZDeveloper <mohamed.hichem.zayani@gmail.com>
2020-12-11 23:26:25 +0100

    Initial Commit

---
+++ src/Client.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+
+    static final int port = 1200;
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("connection request");
+
+        //Create a client socket
+        Socket socket = new Socket("127.0.0.1", port);
+        System.out.println("Connection established");
+
+        // create in / out
+        BufferedReader inClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter outClient = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+
+        String str = "Hello World";
+        outClient.println(str); // send msg
+
+        //close in / out
+        inClient.close();
+        outClient.close();
+
+        // close client socket
+        socket.close();
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    static final int port = 1200;
+
+    public static void main(String[] args) throws Exception {
+
+        // Listen to a specific port
+
+        ServerSocket s = new ServerSocket(port);
+        System.out.println("Waiting for connection");
+        Socket socClient = s.accept(); // Accept a client socket
+        System.out.println("Connection established");
+
+        // Initialize in / out
+
+        BufferedReader inServer = new BufferedReader(new InputStreamReader(socClient.getInputStream()));
+        PrintWriter outServer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socClient.getOutputStream())), true);
+
+        // Read message sent by the client
+        String str = inServer.readLine();
+        String strUpper = str.toUpperCase();
+        System.out.println("The msg was " + strUpper);
+
+        // Close in / out
+        inServer.close();
+        outServer.close();
+
+        // Close client socket
+        socClient.close();
+    }
+}
+
